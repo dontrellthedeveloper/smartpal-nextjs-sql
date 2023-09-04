3cc7f425e30a48ccc811651b1c43579339c69299
dontrellthedeveloper <dontrellknight@gmail.com>
2023-09-04 00:46:56 -0400

    added companion list

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,17 +1,45 @@
-import { SearchInput } from "@/components/search-input"
-import prismadb from "@/lib/prismadb";
+import prismadb from "@/lib/prismadb"
 import { Categories } from "@/components/categories"
 // import { Companions } from "@/components/companions"
+import { SearchInput } from "@/components/search-input"
+import { Companions } from "@/components/companions";
 
+interface RootPageProps {
+  searchParams: {
+    categoryId: string;
+    name: string;
+  };
+};
 
-const RootPage = async () => {
+const RootPage = async ({
+  searchParams
+}: RootPageProps) => {
+  const data = await prismadb.companion.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        search: searchParams.name,
+      },
+    },
+    orderBy: {
+      createdAt: "desc"
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        }
+      }
+    },
+  });
+
   const categories = await prismadb.category.findMany();
 
   return (
     <div className="h-full p-4 space-y-2">
-            <SearchInput />
-       <Categories data={categories}/>
-      {/* <Companions />  */}
+      <SearchInput />
+      <Categories data={categories} />
+      <Companions data={data} />
     </div>
   )
 }
