c45548becd64740672bf735f0f9980c347f25029
dontrellthedeveloper <dontrellknight@gmail.com>
2023-09-03 22:38:08 -0400

    added categories filter

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,16 +1,17 @@
-"use client";
-
 import { SearchInput } from "@/components/search-input"
-// import { Categories } from "@/components/categories"
+import prismadb from "@/lib/prismadb";
+import { Categories } from "@/components/categories"
 // import { Companions } from "@/components/companions"
 
 
-const RootPage = () => {
+const RootPage = async () => {
+  const categories = await prismadb.category.findMany();
+
   return (
     <div className="h-full p-4 space-y-2">
             <SearchInput />
-      {/* <Categories />
-      <Companions /> */}
+       <Categories data={categories}/>
+      {/* <Companions />  */}
     </div>
   )
 }
